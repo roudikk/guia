bf8a2abb8577f8203be9cc9e2d75a1f71192658c
Roudi Korkis Kanaan <roudykk@gmail.com>
2022-01-09 10:15:45 +1100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,15 @@ plugins {
 val composeVersion = "1.1.0-rc01"
 val accompanistVersion = "0.22.0-rc"
 val lottieVersion = "4.2.2"
+val kotlinCoroutinesVersion = "1.6.0"
+val firebaseBomVersion = "29.0.3"
+val materialVersion = "1.6.0-alpha01"
+val activityComposeVersion = "1.4.0"
+val viewModelComposeVersion = "2.4.0"
+val composeMaterial3Version = "1.0.0-alpha02"
+val junitVersion = "4.13.2"
+val jupiterVersion = "5.8.2"
+val truthVersion = "1.1.3"
 
 android {
     compileSdk = 32
@@ -46,22 +55,22 @@ android {
 
 dependencies {
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
 
     // Compose
-    implementation("com.google.android.material:material:1.6.0-alpha01")
+    implementation("com.google.android.material:material:$materialVersion")
     implementation("androidx.compose.material:material:$composeVersion")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling:$composeVersion")
     implementation("androidx.compose.foundation:foundation:$composeVersion")
     implementation("androidx.compose.material:material-icons-core:$composeVersion")
     implementation("androidx.compose.material:material-icons-extended:$composeVersion")
-    implementation("androidx.activity:activity-compose:1.4.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0")
-    implementation("androidx.compose.material3:material3:1.0.0-alpha02")
+    implementation("androidx.activity:activity-compose:$activityComposeVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$viewModelComposeVersion")
+    implementation("androidx.compose.material3:material3:$composeMaterial3Version")
 
     // Firebase
-    implementation(platform("com.google.firebase:firebase-bom:29.0.3"))
+    implementation(platform("com.google.firebase:firebase-bom:$firebaseBomVersion"))
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-crashlytics-ktx")
 
@@ -78,14 +87,10 @@ dependencies {
     implementation(project(":compose-navigator"))
 
     // Test dependencies
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
-    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.8.2")
-    testImplementation("com.google.truth:truth:1.1.3")
-}
-
-kotlin.sourceSets.all {
-    languageSettings.optIn("kotlin.RequiresOptIn")
+    testImplementation("junit:junit:$junitVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter:$jupiterVersion")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:$jupiterVersion")
+    testImplementation("com.google.truth:truth:$truthVersion")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
@@ -93,6 +98,8 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
     }
 }
+
+// Module wide Opt ins for experimental compose / navigator apis
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions.freeCompilerArgs += "-opt-in=com.roudikk.compose_navigator.ExperimentalNavigatorApi"
