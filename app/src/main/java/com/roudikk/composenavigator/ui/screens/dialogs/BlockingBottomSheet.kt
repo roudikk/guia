055fff89465e007130b2f35ad79574dfcca0896c
Roudi Korkis Kanaan <roudykk@gmail.com>
2022-01-09 13:18:54 +1100

    Update warning for blocking bottom sheet

--- app/src/main/java/com/roudikk/composenavigator/ui/screens/dialogs/BlockingBottomSheet.kt
+++ app/src/main/java/com/roudikk/composenavigator/ui/screens/dialogs/BlockingBottomSheet.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
@@ -54,8 +55,9 @@ private fun BlockingBottomSheetContent() {
         Spacer(modifier = Modifier.size(16.dp))
 
         Text(
-            text = "This is currently not working properly until Compose rc02 is released.",
-            fontWeight = FontWeight.Bold
+            text = "This is currently not working properly until Compose 1.1.0-rc02 is released. (Or any version after 1.1.0-rc01)",
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFFD37575)
         )
 
         Spacer(modifier = Modifier.size(16.dp))
