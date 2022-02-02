95ab33dfb40573cbfbbbc0cc378033cb19b91865
Roudi Korkis Kanaan <roudykk@gmail.com>
2022-02-02 21:00:52 +1100

    Add dynamic theming to sample

--- sample/src/main/java/com/roudikk/navigator/sample/ui/theme/Theme.kt
+++ sample/src/main/java/com/roudikk/navigator/sample/ui/theme/Theme.kt
@@ -1,14 +1,15 @@
 package com.roudikk.navigator.sample.ui.theme
 
+import android.os.Build
+import android.util.Log
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import com.google.accompanist.insets.ProvideWindowInsets
+import com.roudikk.navigator.sample.BuildConfig
 import androidx.compose.material.MaterialTheme as MaterialTheme2
 
 private val LightThemeColors = lightColorScheme(
@@ -70,13 +71,16 @@ private val DarkThemeColors = darkColorScheme(
 @Composable
 fun AppTheme(
     useDarkTheme: Boolean = isSystemInDarkTheme(),
+    useDynamicColorScheme: Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S,
     content: @Composable () -> Unit
 ) = ProvideWindowInsets {
+    val context = LocalContext.current
 
-    val colors = if (useDarkTheme) {
-        DarkThemeColors
-    } else {
-        LightThemeColors
+    val colors = when {
+        useDynamicColorScheme && useDarkTheme -> dynamicDarkColorScheme(context)
+        useDynamicColorScheme && !useDarkTheme -> dynamicLightColorScheme(context)
+        useDarkTheme -> DarkThemeColors
+        else -> LightThemeColors
     }
 
     val colors2 = if (useDarkTheme) {
@@ -104,6 +108,7 @@ fun AppTheme(
             onSurface = LightThemeColors.onSurface
         )
     }
+
     MaterialTheme2(
         colors = colors2
     ) {
