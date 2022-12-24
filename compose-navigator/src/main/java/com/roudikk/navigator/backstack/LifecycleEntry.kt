f9d91c2b2e5a490696d114042222d69d2df9785d
Roudi Korkis Kanaan <roudykk@gmail.com>
2022-12-24 11:23:08 +1100

    st

--- compose-navigator/src/main/java/com/roudikk/navigator/backstack/LifecycleEntry.kt
+++ compose-navigator/src/main/java/com/roudikk/navigator/backstack/LifecycleEntry.kt
@@ -1,21 +1,27 @@
 package com.roudikk.navigator.backstack
 
 import android.app.Application
+import android.os.Bundle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.saveable.SaveableStateHolder
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.platform.LocalSavedStateRegistryOwner
+import androidx.lifecycle.HasDefaultViewModelProviderFactory
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleRegistry
+import androidx.lifecycle.SavedStateViewModelFactory
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelStore
+import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.savedstate.SavedStateRegistry
 import androidx.savedstate.SavedStateRegistryController
+import androidx.savedstate.SavedStateRegistryOwner
 import com.roudikk.navigator.core.BackStackEntry
 import com.roudikk.navigator.core.NavigationNode
+import kotlin.properties.Delegates
 
 /**
  * [LifecycleEntry] for a [BackStackEntry].
@@ -29,13 +35,15 @@ import com.roudikk.navigator.core.NavigationNode
 internal class LifecycleEntry(
     val backStackEntry: BackStackEntry,
     application: Application?,
-    viewModelStore: ViewModelStore,
-    saveableStateHolder: SaveableStateHolder,
-) : BackStackLifecycleOwner(viewModelStore, application, saveableStateHolder) {
+    private val viewModelStore: ViewModelStore,
+    internal val saveableStateHolder: SaveableStateHolder,
+) : ViewModelStoreOwner,
+    LifecycleOwner,
+    SavedStateRegistryOwner,
+    HasDefaultViewModelProviderFactory {
 
-    override val id get() = backStackEntry.id
-    override val lifecycleRegistry = LifecycleRegistry(this)
-    override val savedStateRegistryController = SavedStateRegistryController.create(this)
+    private val lifecycleRegistry = LifecycleRegistry(this)
+    private val savedStateRegistryController = SavedStateRegistryController.create(this)
 
     override fun equals(other: Any?): Boolean {
         return this.id == (other as? LifecycleEntry)?.id
@@ -43,6 +51,41 @@ internal class LifecycleEntry(
 
     override fun hashCode(): Int {
         return id.hashCode()
+    }
+
+    internal var navHostLifecycleState by Delegates.observable(Lifecycle.State.INITIALIZED) { _, _, _ ->
+        updateLifecycleRegistry()
+    }
+
+    internal var maxLifecycleState by Delegates.observable(Lifecycle.State.INITIALIZED) { _, _, _ ->
+        updateLifecycleRegistry()
+    }
+
+    internal val savedStateProvider = SavedStateRegistry.SavedStateProvider {
+        Bundle().also { bundle ->
+            savedStateRegistryController.performSave(bundle)
+        }
+    }
+
+    private val defaultFactory by lazy {
+        SavedStateViewModelFactory(application, this)
+    }
+
+    override val savedStateRegistry: SavedStateRegistry
+        get() = savedStateRegistryController.savedStateRegistry
+
+    override fun getViewModelStore() = viewModelStore
+
+    override fun getLifecycle(): Lifecycle = lifecycleRegistry
+
+    override fun getDefaultViewModelProviderFactory() = defaultFactory
+
+    private fun updateLifecycleRegistry() {
+        lifecycleRegistry.currentState = minOf(maxLifecycleState, navHostLifecycleState)
+    }
+
+    internal fun restoreState(savedState: Bundle) {
+        savedStateRegistryController.performRestore(savedState)
     }
 }
 
@@ -59,3 +102,13 @@ internal fun LifecycleEntry.LocalProvider(
 ) {
     SaveableStateProvider(content)
 }
+
+internal val LifecycleEntry.id: String
+    get() = backStackEntry.id
+
+@Composable
+internal fun LifecycleEntry.SaveableStateProvider(content: @Composable () -> Unit) =
+    saveableStateHolder.SaveableStateProvider(
+        key = id,
+        content = content
+    )
